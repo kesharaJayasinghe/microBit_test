a269e426cdc8a3add9c75cc87c7ffe9ac9bd1bf5
Keshara Jayasinghe <kesharalahiru1@gmail.com>
2017-12-19 09:53:27 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
+enum ledPtrn{
+    //% block="P1"
+    pattern1,
+    //% block="P2"
+    pattern2,
+    //% block="P3"
+    pattern3
+}
 
+/**
+ * LED blink patterns
+ */
+//% color="#2c3e50" weight=10
+namespace ledPkg{
+    /**
+     * Set time values
+     * @param VR1 variable1
+     * @param VR2 variable2
+     */
+    //% blockId=LED_TEST block="ptrn VR1 %VR1|VR2 %VR2|VR3 %VR3"
+    export function ptrn(VR1: ledPtrn, VR2 = 10): number {
+        const value = 0
+        const output = (value * 10)
+        
+        switch (VR1) {
+            case ledPtrn.pattern1: value = 1;
+            case ledPtrn.pattern2: value = 2;
+            case ledPtrn.pattern3: value = 3;
+            default: value = 0;    
+        }
+    }
+}
